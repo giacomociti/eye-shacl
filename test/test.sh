738b4f5a3315e02e2bd4a0dd6aeec343d270d342
Giacomo Citi <giacomo.citi@zazuko.com>
2024-07-04 19:57:25 +0200

    fix:  missing backward rule for sh:closed

--- test/test.sh
+++ test/test.sh
@@ -36,6 +36,7 @@ files=(
     "custom/test19.ttl"
     "custom/test20.ttl"
     "custom/test21.ttl"
+    "custom/test22.ttl"
 
     # https://github.com/w3c/data-shapes/tree/gh-pages/data-shapes-test-suite/tests/core
 
