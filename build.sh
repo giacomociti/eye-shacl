354c180a312a331124e0c2b2eb5310d6caa51774
Giacomo Citi <giacomo.citi@zazuko.com>
2024-08-26 09:38:37 +0200

    simplify build

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-rm -f dist/*.n3
+rm -f dist/compile.n3
+rm -f dist/validate.n3
 eye --nope --quiet --pass-merged rules/compile/*.n3 --output dist/compile.n3
 eye --nope --quiet --pass-merged rules/validate/*.n3 --output dist/validate.n3
-cp rules/report.n3 dist/report.n3
-cp rules/imports.n3 dist/imports.n3
-cp rules/shapesGraph.n3 dist/shapesGraph.n3
+curl https://raw.githubusercontent.com/eyereasoner/lib-rdfs/main/lib-rdfs.n3 --output dist/lib-rdfs.n3
--- dist/eye-shacl-validate.sh
+++ dist/eye-shacl-validate.sh
@@ -2,7 +2,6 @@
 
 dir=$(dirname $0)
 validate="$dir/validate.n3"
-shapesGraph="$dir/shapesGraph.n3"
 report="$dir/report.n3"
 
-eye --nope --quiet --query $report $validate $shapesGraph $1 --turtle $2 
+eye --nope --quiet --query $report $validate $1 --turtle $2 
