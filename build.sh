0b4033de8ed580813b2688066255c63bde4014f3
Giacomo Citi <giacomo.citi@zazuko.com>
2024-09-27 15:39:55 +0200

    improve sh:shapesGraph

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -f dist/compile.n3
 rm -f dist/validate.n3
-eye --nope --quiet --pass-merged rules/compile/*.n3 --output dist/compile.n3
-eye --nope --quiet --pass-merged rules/validate/*.n3 --output dist/validate.n3
+cat rules/compile/*.n3 > dist/compile.n3
+cat rules/validate/*.n3 > dist/validate.n3
 curl https://raw.githubusercontent.com/eyereasoner/lib-rdfs/main/lib-rdfs.n3 --output dist/lib-rdfs.n3
