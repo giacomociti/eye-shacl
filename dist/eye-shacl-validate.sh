71c0a5c48653c702c9232676b9b00e3d09d5143f
Giacomo Citi <giacomo.citi@zazuko.com>
2026-04-16 13:00:02 +0200

    draft validate with eyeling

--- dist/eye-shacl-validate.sh
+++ dist/eye-shacl-validate.sh
@@ -4,4 +4,11 @@ dir=$(dirname $0)
 validate="$dir/validate.n3"
 report="$dir/report.n3"
 
-eye --nope --quiet --query $report $validate $1 --turtle $2 
+# todo: replace with eyeling
+# eye --nope --quiet --query $report $validate $1 --turtle $2 
+
+# trim prefix file:// from $1 and $2
+file1=$(echo $1 | sed 's|^file://||')
+file2=$(echo $2 | sed 's|^file://||')
+
+cat $report $validate $file1 $file2 | npx eyeling
