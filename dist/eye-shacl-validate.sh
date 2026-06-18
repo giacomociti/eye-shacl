273eb31d1c86040692f5feb1f9a8432a2b65f186
Giacomo Citi <giacomo.citi@zazuko.com>
2026-06-18 18:31:29 +0200

    avoid sed

--- dist/eye-shacl-validate.sh
+++ dist/eye-shacl-validate.sh
@@ -4,8 +4,4 @@ dir=$(dirname $0)
 validate="$dir/validate.n3"
 # report="$dir/report.n3"
 
-# trim prefix file:// from $1 and $2
-file1=$(echo $1 | sed 's|^file://||')
-file2=$(echo $2 | sed 's|^file://||')
-
-npx eyeling $validate $file1 $file2
+npx eyeling $validate $1 $2
