e2616a191ae2c833655154e7362da3e22387744e
Giacomo Citi <giacomo.citi@zazuko.com>
2026-06-19 08:58:58 +0200

    more eyeling

--- test/testFile.sh
+++ test/testFile.sh
@@ -4,4 +4,5 @@ eye --nope --quiet --query report-expected.n3 ../rules/compile/cbd.n3 --turtle $
 # eye --nope --quiet --strings testFile.n3 $1 | sh > test-actual.ttl
 npx eyeling testFile.n3 $1 | sh > test-actual.ttl
 eye --nope --quiet --query report-actual.n3 path-split.n3 test-actual.ttl --output test-actual-report.ttl
-eye --nope --quiet --pass check.n3 | grep 'isomorphic' | wc -l
+# eye --nope --quiet --pass check.n3 | grep 'isomorphic' | wc -l
+npx eyeling check.n3 | grep 'isomorphic' | wc -l
